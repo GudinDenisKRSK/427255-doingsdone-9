f9172a2e0d9988bda4e0f86d87b78095149cf22c
Denis <gudindenis@rambler.ru>
2019-05-15 15:29:30 +0700

    Исправлены замечания.

--- schema.sql
+++ schema.sql
@@ -1,33 +1,33 @@
-CREATE DATABASE doingsdones
+CREATE DATABASE 427255—doingsdone—9
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-USE doingsdones;
+USE 427255—doingsdone—9;
 
-create table Project (
+create table project (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	project_name VARCHAR(128),
 	user_id INT UNSIGNED NOT NULL
 );
 
-create table Task (
+create table task (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	task_name VARCHAR(128) NOT NULL,
 	create_at DATETIME DEFAULT NOW(),
-	Deadline_at DATETIME,
+	deadline_at DATETIME,
 	done_at DATETIME,
 	file_task VARCHAR(128),
 	project_id INT UNSIGNED NOT NULL,
 	user_id INT UNSIGNED NOT NULL,
-	status BOOLEAN DEFAULT FALSE
+	status TINYINT(1) DEFAULT 0
 );
 
-create table User (
+create table user (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	name_user VARCHAR(128) NOT NULL,
 	email VARCHAR(128) NOT NULL ,
 	password VARCHAR(255) NOT NULL,
-	Register_at DATETIME NOT NULL DEFAULT NOW()
+	register_at DATETIME NOT NULL DEFAULT NOW()
 );
 
 CREATE UNIQUE INDEX email ON User(email);
